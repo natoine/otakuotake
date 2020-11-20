d028c26b1121bd2893c8b6ff99314aef0b6fd8a8
natoine <antoineseilles@gmail.com>
2020-11-20 17:16:21 +0100

    now get a node serveur

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-coucou
+'use strict'
+
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000 ;
+
+app.get("/", function(request, response){
+    console.log('quelquun me parle !');
+    response.send("hello tout va bien");
+})
+
+app.listen(port, function () {
+    console.log('Serveur listening on port ' + port);
+});
