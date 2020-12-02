f6fe8e49ad92193f2138ef23ed83c9e14926d804
natoine <antoineseilles@gmail.com>
2020-12-02 17:05:21 +0100

    now connect to mongolab db ... but do nothing more

--- index.js
+++ index.js
@@ -6,12 +6,24 @@ const app = express();
 const port = process.env.PORT || 3000 ;
 var nbvisit = 0 ;
 
+const mongodb = require('mongodb');
+const dbuser = "username" ;
+const dbpass = "password";
+
+const urimongo = `mongodb://${dbuser}:${encodeURIComponent(dbpass)}@ds139341.mlab.com:39341/otakuotake`;
+console.log("uri mongodb", urimongo);
+
 //serves static files
 app.use(express.static('resources/public'));
 
 app.get("/whatismyname", function(request, response) {
-    response.send("Broly");
-})
+    mongodb.MongoClient.connect(urimongo, function (err, db) {
+        if(err) console.log("error" , err);
+        else {
+            console.log("connected !");
+        }
+    });
+});
 
 app.listen(port, function () {
     console.log('Serveur listening on port ' + port);
