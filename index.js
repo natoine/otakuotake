ab82089c84714648b79b18311f0217196d0e6231
natoine <antoineseilles@gmail.com>
2020-12-18 12:52:35 +0100

    no more console log in index.js

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ var nbvisit = 0 ;
 const mongodb = require('mongodb');
 
 const urimongo = require("./resources/secret/databaseconfig.js").url ;
-console.log("uri mongodb", urimongo);
 
 //serves static files
 app.use(express.static('resources/public'));
@@ -18,7 +17,6 @@ app.get("/get2randomcharacters", function(request, response) {
     mongodb.MongoClient.connect(urimongo, { useUnifiedTopology: true }, function (err, client) {
         if(err) console.log("error" , err);
         else {
-            //console.log("connected !");
             client.db("otakuotake").collection("characters").find().toArray(function(err, items) {
                 if(err) throw err;
                 var dblength = items.length;
@@ -27,7 +25,6 @@ app.get("/get2randomcharacters", function(request, response) {
                 while(nb2==nb1){
                     nb2 = Math.floor(Math.random()*dblength);
                 }
-                //console.log("nb", nb);    
                 response.send([items[nb1], items[nb2]]);          
             });
         }
@@ -38,12 +35,10 @@ app.get("/getrandomcharacter", function(request, response) {
     mongodb.MongoClient.connect(urimongo, { useUnifiedTopology: true }, function (err, client) {
         if(err) console.log("error" , err);
         else {
-            //console.log("connected !");
-            client.db("otakuotake").collection("characters").find().toArray(function(err, items) {
+           client.db("otakuotake").collection("characters").find().toArray(function(err, items) {
                 if(err) throw err;
                 var dblength = items.length;
                 var nb = Math.floor(Math.random()*dblength);
-                //console.log("nb", nb);    
                 response.send(items[nb]);          
             });
         }
