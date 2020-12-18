19e1ea19ef3debfe2d5f3cb7ca6ad2d54c99cd13
ion239 <bressand.guillaume1309@gmail.com>
2020-12-18 12:50:51 +0100

    2characters random

--- index.js
+++ index.js
@@ -14,6 +14,26 @@ console.log("uri mongodb", urimongo);
 //serves static files
 app.use(express.static('resources/public'));
 
+app.get("/get2randomcharacters", function(request, response) {
+    mongodb.MongoClient.connect(urimongo, { useUnifiedTopology: true }, function (err, client) {
+        if(err) console.log("error" , err);
+        else {
+            //console.log("connected !");
+            client.db("otakuotake").collection("characters").find().toArray(function(err, items) {
+                if(err) throw err;
+                var dblength = items.length;
+                var nb1 = Math.floor(Math.random()*dblength);
+                var nb2 = Math.floor(Math.random()*dblength);
+                while(nb2==nb1){
+                    nb2 = Math.floor(Math.random()*dblength);
+                }
+                //console.log("nb", nb);    
+                response.send([items[nb1], items[nb2]]);          
+            });
+        }
+    });
+});
+
 app.get("/getrandomcharacter", function(request, response) {
     mongodb.MongoClient.connect(urimongo, { useUnifiedTopology: true }, function (err, client) {
         if(err) console.log("error" , err);
